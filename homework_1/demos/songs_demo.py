b6c8f15a7d7af958ee1dbdabc4a9d59056974787
Frano Rajič <frano.rajich@gmail.com>
2020-11-06 16:07:15 +0100

    make songs demo more efficient

--- homework_1/demos/songs_demo.py
+++ homework_1/demos/songs_demo.py
@@ -1,16 +1,17 @@
 import csv
 import io
+import numpy as np
+from tqdm import tqdm
 
-from models.Lsh import Lsh
-from models.MinHashing import MinHashing
+from models.CompareSignatures import CompareSignatures
 from models.Shingling import Shingling
 
 SONGS_DATASET_PATH = "../../datasets/scraped_songs.csv"
 # SONGS_DATASET_PATH = "../../datasets/scraped_songs_2.csv"
 PICKLE_PATH = "../../datasets/dump"
-K = 9
+K = 5
 N = 99
-BAND_WIDTH = 3
+BAND_WIDTH = 20
 LSH_THRESHOLDS = [0.1, 0.3, 0.7, 0.05, 0.01]
 SEED = 72
 HASH = hash
@@ -27,13 +28,35 @@ if __name__ == '__main__':
             names.append(row[0] + " -- " + row[1] + " -- url:" + row[4])
             hash_brownies.append(Shingling(io.StringIO(row[2]), K, HASH, universe).return_hashed_shingles())
 
-    print(f"got {len(hash_brownies)} brownies")
+    print(f"got {len(universe)} brownies in universe")
     print("now signatures")
     universe_list = list(universe)
-    signatures = [MinHashing(N, brownie, universe_list, SEED).return_signature() for brownie in hash_brownies]
+    signatures = []
+    np.random.seed(SEED)
+    permutations = [np.random.permutation(np.arange(len(universe_list))) for _ in range(N)]
+    for brownie in tqdm(hash_brownies):
+        signature = []
+        for perm in permutations:
+            for i in range(len(perm)):
+                if universe_list[perm[i]] in brownie:
+                    signature.append(i)
+                    break
+        signatures.append(signature)
 
     print("lsh")
-    for threshold in LSH_THRESHOLDS:
-        candidates = Lsh(signatures, BAND_WIDTH, threshold, HASH).ret_candidates()
-        candidate_names = [(names[candidate[0]], names[candidate[1]]) for candidate in candidates]
-        print(f"Candidates for threshold {threshold}:\n{candidate_names}\n")
+    number_of_bands = len(signatures[0]) // BAND_WIDTH
+    lsh_matrix = []
+    for signature in signatures:
+        lsh_signature = []
+        for band_id in range(number_of_bands):
+            band = tuple(signature[band_id * BAND_WIDTH: band_id * BAND_WIDTH + BAND_WIDTH])
+            lsh_signature.append(HASH(band))
+        lsh_matrix.append(lsh_signature)
+    threshold_to_pairs = {th: list() for th in LSH_THRESHOLDS}
+    for i in range(len(lsh_matrix)):
+        for j in range(i + 1, len(lsh_matrix)):
+            comparison = CompareSignatures(lsh_matrix[i], lsh_matrix[j])
+            for threshold in LSH_THRESHOLDS:
+                if comparison.similarity > threshold:
+                    threshold_to_pairs[threshold].append((names[i], names[j], comparison.similarity))
+    print(f"Threshold to pairs: {threshold_to_pairs}\n")
